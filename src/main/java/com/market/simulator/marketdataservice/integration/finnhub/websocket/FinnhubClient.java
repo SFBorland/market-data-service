dc2141e4ae34820644fbaa786407745ba501daaa
Sean Borland <seanfborland@gmail.com>
2026-02-10 10:43:01 -0500

    open market responses

--- src/main/java/com/market/simulator/marketdataservice/integration/finnhub/websocket/FinnhubClient.java
+++ src/main/java/com/market/simulator/marketdataservice/integration/finnhub/websocket/FinnhubClient.java
@@ -17,20 +17,21 @@ public class FinnhubClient extends WebSocketClient {
   public void onOpen(ServerHandshake handshakedata) {
     System.out.println("Connected to Finnhub");
     // Subscribe to symbols as soon as the connection opens
-    this.send("{\"type\":\"subscribe\",\"symbol\":\"BINANCE:BTCUSDT\"}");
 //    this.send("{\"type\":\"subscribe\",\"symbol\":\"BINANCE:BTCUSDT\"}");
+    this.send("{\"type\":\"subscribe\",\"symbol\":\"AAPL\"}");
+//    this.send("{\"type\":\"subscribe\",\"symbol\":\"NVDA\"}");
   }
 
   @Override
   public void onMessage(String message) {
     try {
       FinnhubTradeMessage tradeMsg = mapper.readValue(message, FinnhubTradeMessage.class);
-
+System.out.println("Message: " + message);
       if ("trade".equals(tradeMsg.type) && tradeMsg.data != null) {
         for (FinnhubTradeMessage.TradeData trade : tradeMsg.data) {
-          System.out.println("Trade: " + trade);
-          System.out.printf("Symbol: %s | Price: %.2f | Volume: %f%n",
-              trade.symbol, trade.price, trade.volume);
+//          System.out.println("Message: " + message);
+//          System.out.printf("Symbol: %s | Price: %.2f | Volume: %f | TimeStamp: %s%n",
+//              trade.symbol, trade.price, trade.volume, trade.timestamp);
 
           // Process each trade (e.g., push to Kafka, save to DB, or update cache)
         }
